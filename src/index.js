82c0f0fe8f44b905c8caa800736bc3262fc7c3e4
RhoLion <rholeon@gmail.com>
2022-01-26 19:35:12 -0600

    creditCardValidator
    initial credit card validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+//Acá escribirás todo el código que tenga que ver con la interacción del DOM
+//(seleccionar, actualizar y manipular elementos del DOM y eventos).
+//Es decir, en este archivo deberás invocar las funciones isValid y maskify
+// según sea necesario para actualizar el resultado en la pantalla (UI).
+
+ 
+function isCardValid() {
+  
+  let cardNumber = document.getElementById("cardNumberOrigin");
+  let sizeValid = isCardSizeValid(cardNumber.value);
+  if (sizeValid == true){
+    let valid = validator.isValid(cardNumber.value);
+    updateResult(valid);
+  } else{
+    document.getElementById("incompleteCard").innerText =
+    "La tarjeta debe tener de 13 a 20 dígitos";
+    document.getElementById("invalidResult").innerText = "";
+    document.getElementById("validResult").innerText = "";
+  }
+  
+}
+
+function isCardSizeValid(cardNumber) {
+  
+  if (cardNumber.length < 11 || cardNumber.length > 20) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function updateResult(valid) {
+  if (valid == true) {
+    document.getElementById("validResult").innerText =
+      "Tarjeta válida, puede continuar con la contratación";
+      document.getElementById("invalidResult").innerText = "";
+      document.getElementById("incompleteCard").innerText = "";
+  } else {
+    document.getElementById("invalidResult").innerText =
+      "Tarjeta invalida, intente nuevamente";
+      document.getElementById("validResult").innerText = "";
+      document.getElementById("incompleteCard").innerText = "";
+  }
+}
+
+function maskifyEvent(e) {
+    let cardNumberInput = document.getElementById("cardNumberInput");
+    let cardNumberArray = Array.from(e.target.value);
+    
+    if (e.key !== undefined && e.keyCode >= 48 && e.keyCode <= 57) {
+      let newMaskify = validator.maskify(cardNumberInput.value);
+      //asignar el valor enmascarado al input
+      document.getElementById("cardNumberOrigin").value += e.key;
+      cardNumberInput.value = newMaskify;
+    } else {
+      cardNumberArray.splice(-1); 
+      cardNumberInput.value = cardNumberArray.join('');
+    }
+}
+
+let cardNumberInput = document.getElementById("cardNumberInput");
+cardNumberInput.addEventListener("keyup", maskifyEvent);
+
+let submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", isCardValid);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,56 @@
 const validator = {
-  // ...
-};
+
+  isValid(cardNumber) {
+
+     //colocamos los números que ingresó el usuario en orden inverso y los cambiamos a string
+ 
+     let num = Array.from(cardNumber);
+     num.reverse();
+     let x = "";
+     //con los números invertidos corremos arreglo para aplicar multiplicación
+     //en posición par y validamos si el número es mayor a 10. (si se cumple le restamos 9)
+     for (let i = 0; i < num.length; i++) {
+       if ((i+1) % 2 == 0 && i != 0) {
+         if (num[i] * 2 >= 10) {
+           x = num[i] * 2 - 9;
+           num[i] = x;
+         } else {
+           num[i] *= 2;
+         }
+       }
+     }
+ 
+     //creamos otro arreglo para sumar los números impares y los nuevos digitos de primer arreglo
+     //y los cambiamos a números
+     let total = 0;
+     for (let i = 0; i < num.length; i++) {
+       total += Number(num[i]);
+     }
+     if (total % 10 == 0){
+       return true;
+     }else{
+       return false;
+     }
+     
+ },
+ 
+ 
+  maskify(cardNumberInput) {
+
+   let cardNumberArray = Array.from(cardNumberInput);
+     let newMaskify = "";  
+     for (let i = 0; i < cardNumberArray.length; i++) {
+       if ( i < cardNumberArray.length - 4) {
+         newMaskify += "#";
+       } else {
+         newMaskify += cardNumberArray[i];
+       }
+     }
+   return newMaskify;
+ },
+ 
+}
+
+
 
 export default validator;
