0dd43f20382b9fac042ec5083f3ff42086d48e24
RhoLion <rholeon@gmail.com>
2022-01-28 11:13:23 -0600

    Readme
    correcciones en el ReadMe

--- README.md
+++ README.md
@@ -7,17 +7,18 @@
 * Este es un programa que valida tarjetas de crédito para la adqusición de cursos de inglés de todos los niveles. Al hacer la validación, el usuario podrá continuar con la contratación.
 
 ##Prototipo final
-![Prototipo final](CDMX012-card-validation/img/cardValidation.png)
+![Prototipo final](https://raw.githubsercontent.com/rholion/CDMX012-card-validation/img/cardValidation.png)
 * Investigación UX:
   Programa dirigido a personas interesadas en mejorar su nivel de inglés. El objetivo principal es facilitar la validación de datos de pago, para brindarle un rápido acceso a los distintos cursos.
   
   Al validar la tarjeta le dará la oportunidad a los usuarios de avanzar en la contratación del curso de inglés, permitiendole disponer de tiempo para avanzar en sus objetivos
 
 ##Boceto en papel
-![Boceto en papel](CDMX012-card-validation/img/boceto.png)
+![Boceto en papel](https://raw.githubsercontent.com/rholion/CDMX012-card-validation/img/boceto.png)
 
   Recibí feedback con respecto a los campos a validar, es decir, sólo validar tarjeta.
   Cambio en el tamaño de letras y ayuda con el enmascarado de la tarjeta.
 
 ##Imagen del prototipo final.
-![Prototipo final](CDMX012-card-validation/img/final.png)
+![Prototipo final](https://raw.githubsercontent.com/rholion/CDMX012-card-validation/img/final.png)
+![Diagrama de Flujo](https://raw.githubsercontent.com/rholion/CDMX012-card-validation/img/final.png)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import validator from "./validator.js";
 
-//Acá escribirás todo el código que tenga que ver con la interacción del DOM
-//(seleccionar, actualizar y manipular elementos del DOM y eventos).
-//Es decir, en este archivo deberás invocar las funciones isValid y maskify
-// según sea necesario para actualizar el resultado en la pantalla (UI).
 
  
 function isCardValid() {
-  
+  //invocamos los datos ingresados por el usuario 
   let cardNumber = document.getElementById("cardNumberOrigin");
+  //llamamos a la función que valida el tamaño de los datos ingresados
   let sizeValid = isCardSizeValid(cardNumber.value);
+  //si es correcto el tamaño de los datos, invocamos a la función isValid
   if (sizeValid == true){
     let valid = validator.isValid(cardNumber.value);
     updateResult(valid);
@@ -32,6 +30,7 @@ function isCardSizeValid(cardNumber) {
 }
 
 function updateResult(valid) {
+  //si la validación retorna verdadero, enviamos mensaje al usuario
   if (valid == true) {
     document.getElementById("validResult").innerText =
       "Tarjeta válida, puede continuar con la contratación";
@@ -46,22 +45,26 @@ function updateResult(valid) {
 }
 
 function maskifyEvent(e) {
+  //invocamos los datos ingresados por el usuario 
     let cardNumberInput = document.getElementById("cardNumberInput");
-    let cardNumberArray = Array.from(e.target.value);
+    let cardNumberArray = Array.from(e.target.value);//hacemos referencia al valor del objeto
     
+  //si validamos que el usuario ingrese sólo números, entonces llamamos a nuestra función Maskify
     if (e.key !== undefined && e.keyCode >= 48 && e.keyCode <= 57) {
       let newMaskify = validator.maskify(cardNumberInput.value);
-      //asignar el valor enmascarado al input
+      //asignamos el valor enmascarado al input
       document.getElementById("cardNumberOrigin").value += e.key;
       cardNumberInput.value = newMaskify;
     } else {
+    //si el usuario ingresa datos distintos de [0-9] lo removemos y unimos los datos existentes
       cardNumberArray.splice(-1); 
       cardNumberInput.value = cardNumberArray.join('');
     }
 }
 
+//invocamos los datos ingresados por el usuario y agregamos el evento
 let cardNumberInput = document.getElementById("cardNumberInput");
 cardNumberInput.addEventListener("keyup", maskifyEvent);
-
+//agregamos evento click al input button 
 let submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", isCardValid);
